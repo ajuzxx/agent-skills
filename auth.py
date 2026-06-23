27d8f4afc843c90898dfd2695f64ae1f2da986b7
ajuzxx <ajasmathew2003@gmail.com>
2026-06-23 20:28:26 +0530

    feat(auth): add google login functionality

--- auth.py
+++ auth.py
@@ -1,2 +1,24 @@
+import os
+from google_auth_oauthlib.flow import Flow
+
 def login():
-    pass
+    """Starts the Google OAuth2 flow by generating the authorization URL."""
+    client_secrets_path = os.environ.get('GOOGLE_CLIENT_SECRETS_PATH', 'client_secret.json')
+    
+    flow = Flow.from_client_secrets_file(
+        client_secrets_path,
+        scopes=[
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+            'openid'
+        ],
+        redirect_uri='http://localhost:8080/callback'
+    )
+    
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true'
+    )
+    
+    return authorization_url, state
+
